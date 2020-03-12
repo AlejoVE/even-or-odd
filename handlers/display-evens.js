adbd87a7201ee507279da3b5db3113f2f84ec91b
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-03-12 22:51:18 +0100

    evens handler and listener

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,6 +1,17 @@
 function evensHandler() {
   debugger;
   // write me!
+  strings.displayed = 'evens';
+
+  const displayed = strings.displayed;
+  const array = strings[displayed];
+
+  const evensList = renderList(array, displayed);
+
+  const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  
+  listContainer.appendChild(evensList);
 
   // log user interaction: handler name, user input, new state
   log.push({
