a889202d959f2f049d46b055cbfd3e9aedcd6153
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-03-12 17:04:37 +0100

    I made the handler and listener code for the save button

--- handlers/save.js
+++ handlers/save.js
@@ -1,6 +1,17 @@
 function saveHandler() {
   debugger;
   // write me!
+  const newInput = document.getElementById("input").value.trim();
+
+  if (newInput == ""){
+    alert ("The field cannot be empty");
+  } else if( isNaN(newInput)){
+    strings.NaNy.push(newInput);
+  } else if(newInput  % 2 === 0){
+    strings.evens.push(newInput);
+  } else{
+    strings.odds.push(newInput);
+  }
 
   // log user interaction: handler name, user input, new state
   log.push({
