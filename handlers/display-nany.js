fd46db7d22f4c1a593410cb34acf52415e8f3867
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-03-12 23:16:22 +0100

    nany handler and listener

--- handlers/display-nany.js
+++ handlers/display-nany.js
@@ -1,7 +1,17 @@
 function nanyHandler() {
   debugger;
   // write me!
+  strings.displayed = 'NaNy';
 
+  const displayed = strings.displayed;
+  const array = strings[displayed];
+
+  const evensList = renderList(array, displayed);
+
+  const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  
+  listContainer.appendChild(evensList);
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'NaNy',
